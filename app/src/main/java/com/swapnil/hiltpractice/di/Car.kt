87454ba5247d986f1a510ce2286d6931d146f68e
swapnil7000 <swapnil.bhojwani789@gmail.com>
2021-10-02 23:05:18 +0530

    Constructor injection added in car class

--- app/src/main/java/com/swapnil/hiltpractice/di/Car.kt
+++ app/src/main/java/com/swapnil/hiltpractice/di/Car.kt
@@ -3,10 +3,8 @@ package com.swapnil.hiltpractice.di
 import android.util.Log
 import javax.inject.Inject
 
-class Car @Inject constructor(private val engine: Engine,private val wheel: Wheel) {
+class Car @Inject constructor() {
     fun getCar(){
-        engine.getEngine()
-        wheel.getWheel()
         Log.e("main","Car mil gayi...")
     }
 }
